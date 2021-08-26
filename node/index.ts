669133079aeb5d44fe74d2cbd99010f2b5647095
walker015 <walker.alvesmarques@gmail.com>
2021-08-26 19:46:48 -0300

    fist commit

---
+++ node/index.ts
@@ -0,0 +1,36 @@
+import type { ClientsConfig, ServiceContext } from '@vtex/api'
+import { method, Service } from '@vtex/api'
+
+import { Clients } from './clients'
+import { status } from './middlewares/status'
+import { statusPost } from './middlewares/statusPost'
+
+const TIMEOUT_MS = 800
+
+
+const clients: ClientsConfig<Clients> = {
+  implementation: Clients,
+  options: {
+    default: {
+      retries: 2,
+      timeout: TIMEOUT_MS,
+    },
+  },
+}
+
+declare global {
+  type Context = ServiceContext<Clients>
+
+}
+
+export default new Service({
+  clients,
+  routes: {
+    status: method({
+      GET: [status],
+    }),
+    statusPost: method({
+      POST: [statusPost]
+    })
+  },
+})
